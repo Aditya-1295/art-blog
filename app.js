5cd16db548fc2c839760a3587b5182833f45354d
Aditya-1295 <adityaguptamessi@gmail.com>
2021-08-17 17:56:57 +0530

    routing complete

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("aaa");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
+app.get("/contact",function(req,res){
+    res.render("contact")
+})
+
+
+app.listen(3000, function(){
+    console.log("server has started on port 3000");
+});
